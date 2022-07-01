4a23e527b3a369aaeef04b834e06d5683c6eb062
Anton_Shalukho <grinfrend2@gmail.com>
2022-07-01 15:38:31 +0200

    delete KeyA.ts

--- src/components/IconsDesk.tsx
+++ src/components/IconsDesk.tsx
@@ -1,20 +1,12 @@
-import React, { ReactElement, useEffect, useMemo, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import closeSVG from '../png/cross.png'
 import { toggleIconsWrap } from '../store/slices/ChangeIconSlice'
 import getIcons from '../API/GetIcons'
 import { selectorChangeIcon } from '../store'
-import axios from 'axios'
 import Icons from '../UI/Icons'
 import { changeIconSrc } from '../store/slices/AccountIconSlice'
-import { createAction } from '@reduxjs/toolkit'
 
-// const [fetchPost, isPostsLoading, postError] = useFetching(async (limit, page) => {
-//   const response = await PostService.getAll(limit, page);
-//   setPosts([...posts, ...response.data]);
-//   const totalCount = response.headers['x-total-count'];
-//   setTotalPages(getPageCount(totalCount, limit));
-// })
 
 type IconsObj = {
   image: string
@@ -40,7 +32,6 @@ const IconsDesk = () => {
   }
 
   function newAccountIcon(src: string):void {
-    // const newAction = createAction('NEW_ICON_SRC', (src) => {return payload: src})
     dispatch(changeIconSrc(src))
   }
 
